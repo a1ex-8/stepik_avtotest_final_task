e355d28e259722bd1fe6e0f27cb7e57428ae506d
a1lex-8 <johndoe@example.com>
2020-04-25 20:48:08 +0700

    almost Page Object

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,1 +1,7 @@
-Режим вывода команд на экран (ECHO) включен.
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage): 
+	 def go_to_login_page(self):
+		login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+		login_link.click()
